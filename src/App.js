db07e562f451ab28e6dc6ec55af1a130dcb2c150
henriqueomatos <aghenriquematos@gmail.com>
2022-10-22 16:49:38 -0300

    wip:adding elementos on home carousel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import HomeCarousel from './components/home-carousel/HomeCarousel';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome ReactJS</h1>
+      <HomeCarousel/>
     </div>
   );
 }
---
+++ src/components/home-carousel/HomeCarousel.jsx
@@ -0,0 +1,15 @@
+import 
+
+function HomeCarousel() {
+    return (
+    <>
+        <h2>Melhores ofertas personalizadas</h2>
+        <h1>Queima de Estoque Nike</h1>;
+        <h2>Aqui você encontra as melhores ofertas de acordo com o que você gosta</h2>
+        <button>Ver Ofertas</button>
+        <img src="" alt="" />
+    </>
+    );
+  }
+  
+  export default HomeCarousel;
