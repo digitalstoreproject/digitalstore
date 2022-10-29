84d38dd3edb636250d9815070b756c6310053101
henriqueomatos <aghenriquematos@gmail.com>
2022-10-29 16:45:33 -0300

    pratica dia 29 de out

--- src/components/home-carousel/HomeCarousel.jsx
+++ src/components/home-carousel/HomeCarousel.jsx
@@ -1,14 +1,21 @@
-import 
+
 
 function HomeCarousel() {
     return (
-    <>
-        <h2>Melhores ofertas personalizadas</h2>
+    <section className="mainSlide">
+        <div className="textSlide">
+
+        </div>
+        <div className="imageSlide">
+
+        </div>
+        {/* <h2>Melhores ofertas personalizadas</h2>
         <h1>Queima de Estoque Nike</h1>;
         <h2>Aqui você encontra as melhores ofertas de acordo com o que você gosta</h2>
         <button>Ver Ofertas</button>
-        <img src="" alt="" />
-    </>
+        <img src="" alt="" /> */}
+    </section>
+
     );
   }
   
